cab8d737f6536711cc5c3c9bed7cc6607ab045f2
kama34 <kamishnikov98@gmail.com>
2024-07-19 17:32:06 +0300

    Update dags

--- services/airflow/dags/data_extract_dag.py
+++ services/airflow/dags/data_extract_dag.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from datetime import datetime, timedelta, date
+from datetime import datetime, timedelta
 import os
 import subprocess
 import yaml
@@ -12,7 +12,7 @@ from src.validate_data import validate_initial_data
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': date.today(),
+    'start_date': datetime(2023, 7, 19),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
--- services/airflow/dags/data_prepare_dag.py
+++ services/airflow/dags/data_prepare_dag.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta, date
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': date.today(),
+    'start_date': datetime(2023, 7, 19),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
