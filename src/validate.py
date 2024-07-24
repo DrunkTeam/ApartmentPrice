c65db4e9e2e475744afe5b0708bd0b4a3c078548
kama <kamishnikov98@gmail.com>
2024-07-24 21:50:17 +0300

    4 phase finish script, but not work

--- src/validate.py
+++ src/validate.py
@@ -1,111 +1,172 @@
-
 # src/validate.py
 
-from data import extract_data  # custom module
-from transform_data import transform_data  # custom module
-from model import retrieve_model_with_alias  # custom module
-from utils import init_hydra  # custom module
 import giskard
 import mlflow
+import os
+import numpy as np
+from model import load_features
+from sklearn.metrics import mean_squared_error
+from hydra import compose, initialize
+import argparse
 
-def validate():
-    cfg = init_hydra()
 
-    version = cfg.test_data_version
+def create_giskard_dataset(data_version, cfg):
+    target_col = cfg.datasets.target_col
+    X_test, y_test = load_features(name="features_target", version=data_version, target_col=target_col)
+    dataset_name = f"sample_{data_version}_giskard"
 
-    df, version = extract_data(version=version, cfg=cfg)
-
-    TARGET_COLUMN = cfg.data.target_cols[0]
-    CATEGORICAL_COLUMNS = list(cfg.data.cat_cols) + list(cfg.data.bin_cols)
-    dataset_name = cfg.data.dataset_name
+    df = X_test.copy()
+    df[target_col] = y_test
 
     giskard_dataset = giskard.Dataset(
         df=df,
-        target=TARGET_COLUMN,
+        target=target_col,
         name=dataset_name,
-        cat_columns=CATEGORICAL_COLUMNS
     )
+
+    return giskard_dataset, df
+
+
+def load_model(model_name, model_version, feature_cols, cfg):
+    model_uri = f"models:/{model_name}/{model_version}"
+    model = mlflow.sklearn.load_model(model_uri=model_uri)
+
+    target_col = cfg.datasets.target_col
+
+    def predict_fn(X):
+        y_pred = model.predict(X).flatten()
+        return y_pred
+
+    giskard_model = giskard.Model(
+        model=predict_fn,
+        feature_names=feature_cols,
+        target=target_col,
+        model_type="regression",
+        name=f'{model_name}_v{model_version}',
+    )
+
+    return giskard_model
+
+
+def run_giskard_scan(giskard_model, giskard_dataset, model_name, model_version, data_version, cfg):
+    root_dir = cfg.paths.root_path
+    scan_path = os.path.join(root_dir, "reports",
+                             f"test_suite_{model_name}_v{model_version}_sample_{data_version}.html")
+    scan = giskard.scan(giskard_model, giskard_dataset)
+    scan.to_html(scan_path)
+
+    return scan
+
+
+def run_giskard_tests(giskard_model, giskard_dataset, model_name, model_version,
+                      data_version, mae_threshold=10):
+    suite_name = f"test_suite_{model_name}_v{model_version}_sample_{data_version}"
+    test_suite = giskard.Suite(name=suite_name)
+
+    def rmse_test_fn(model, data):
+        y_true = data.df[data.target].values.astype(np.float32).reshape(-1, 1)
+        y_pred = model.predict(data).raw.reshape(-1, 1)
+        mae = np.sqrt(mean_squared_error(y_true, y_pred))
+        return giskard.TestResult(
+            metric_name="MAE",
+            metric=mae,
+            passed=mae < mae_threshold,
+        )
+
+    test_suite.add_test(rmse_test_fn, model=giskard_model, data=giskard_dataset,
+                        display_name=f"MAE Test (Threshold {mae_threshold})", test_id="mae_test")
+
+    test_results = test_suite.run()
+    return test_results
+
+
+def validate_model(model_name, model_alias, model_version,
+                   data_version, mae_threshold, giskard_dataset, cfg):
+    if model_version is None and (model_alias is None or model_alias == ""):
+        raise ValueError("Either model_alias or model_version should be provided")
 
     client = mlflow.MlflowClient()
+    if model_alias is not None and model_alias != "":
+        print("AAAAAAAAAA")
+        print(client.get_latest_versions(model_name))
+        model_version = client.get_model_version_by_alias(model_name, model_alias).version
 
-    challenger1 = retrieve_model_with_alias('model_name', 'challenger1')
-    challenger2 = retrieve_model_with_alias('model_name', 'challenger2')
+    feature_cols = giskard_dataset.df.columns.tolist()
+    target_col = cfg.datasets.target_col
+    feature_cols.remove(target_col)
 
-    mv1 = client.get_model_version_by_alias(name='model_name', alias='challenger1')
-    mv2 = client.get_model_version_by_alias(name='model_name', alias='challenger2')
+    giskard_model = load_model(model_name, model_version, feature_cols, cfg)
+    scan = run_giskard_scan(giskard_model, giskard_dataset, model_name, model_version, data_version, cfg)
+    test_results = run_giskard_tests(giskard_model, giskard_dataset, model_name, model_version,
+                                     data_version, mae_threshold)
+    return scan, test_results, model_version
 
-    model_version1 = mv1.version
-    model_version2 = mv2.version
 
-    transformer_version = cfg.data_transformer_version
+def validate_all_models(model_name, data_version, mae_threshold, giskard_dataset, cfg):
+    client = mlflow.MlflowClient()
+    num_challengers = cfg.model.num_challengers
+    challenger_aliases = [f"challenger{i}" for i in range(num_challengers)]
 
-    def predict_challenger1(raw_df):
-        X = transform_data(
-            df=raw_df,
-            version=version,
-            cfg=cfg,
-            return_df=False,
-            only_transform=True,
-            transformer_version=transformer_version,
-            only_X=True
-        )
-        return challenger1.predict(X)
+    best_model = None
+    min_issues = float("inf")
 
-    def predict_challenger2(raw_df):
-        X = transform_data(
-            df=raw_df,
-            version=version,
-            cfg=cfg,
-            return_df=False,
-            only_transform=True,
-            transformer_version=transformer_version,
-            only_X=True
-        )
-        return challenger2.predict(X)
+    for challenger_alias in challenger_aliases:
+        try:
+            scan, test_results, model_version = validate_model(model_name, challenger_alias, None,
+                                                               data_version, mae_threshold, giskard_dataset, cfg)
+            if test_results.passed:
+                num_issues = len(scan.issues)
+                if num_issues < min_issues:
+                    best_model = (challenger_alias, model_version)
+                    min_issues = num_issues
+        except Exception as e:
+            print(f"Error validating model {challenger_alias}: {e}")
 
-    giskard_model1 = giskard.Model(
-        model=predict_challenger1,
-        model_type="classification",
-        classification_labels=list(cfg.data.labels),
-        feature_names=df.columns,
-        name='challenger1'
-    )
+    if best_model is not None:
+        print(f"Promoting {best_model} to champion")
+        client.transition_model_version_stage(name=model_name,
+                                              version=best_model[1],
+                                              stage="Production")
 
-    giskard_model2 = giskard.Model(
-        model=predict_challenger2,
-        model_type="classification",
-        classification_labels=list(cfg.data.labels),
-        feature_names=df.columns,
-        name='challenger2'
-    )
+        client.delete_registered_model_alias(name=model_name, alias="champion")
+        client.set_registered_model_alias(name=model_name, alias="champion", version=best_model[1])
 
-    scan_results1 = giskard.scan(giskard_model1, giskard_dataset)
-    scan_results_path1 = f"reports/test_suite_challenger1_{model_version1}_{dataset_name}_{version}.html"
-    scan_results1.to_html(scan_results_path1)
-
-    scan_results2 = giskard.scan(giskard_model2, giskard_dataset)
-    scan_results_path2 = f"reports/test_suite_challenger2_{model_version2}_{dataset_name}_{version}.html"
-    scan_results2.to_html(scan_results_path2)
-
-    test_suite1 = giskard.Suite(name=f"test_suite_challenger1_{model_version1}_{dataset_name}_{version}")
-    test1 = giskard.testing.test_f1(model=giskard_model1, dataset=giskard_dataset, threshold=cfg.model.f1_threshold)
-    test_suite1.add_test(test1)
-
-    test_suite2 = giskard.Suite(name=f"test_suite_challenger2_{model_version2}_{dataset_name}_{version}")
-    test2 = giskard.testing.test_f1(model=giskard_model2, dataset=giskard_dataset, threshold=cfg.model.f1_threshold)
-    test_suite2.add_test(test2)
-
-    test_results1 = test_suite1.run()
-    test_results2 = test_suite2.run()
-
-    if test_results1.passed and test_results1.num_issues < test_results2.num_issues:
-        selected_model = 'challenger1'
-        selected_model_version = model_version1
+        return best_model
     else:
-        selected_model = 'challenger2'
-        selected_model_version = model_version2
+        print("No challenger model passed the tests")
+        return None
 
-    print(f"Selected model: {selected_model} (version: {selected_model_version})")
+
+def main():
+    with initialize(config_path="../configs", job_name="validate", version_base=None):
+        cfg = compose(config_name="main")
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--model-name", type=str, default="gbr", help="Model name (gbr or dtr)",
+                            required=True)
+        parser.add_argument("--model-alias", type=str, default=None, help="Model alias (champion or challenger<i>)")
+        parser.add_argument("--model-version", type=int, default=None, help="Model version (1, 2, etc.)")
+        parser.add_argument("--data-version", type=str, default="1", help="Dataset version", required=True)
+        parser.add_argument("--mae-threshold", type=float, default=10, help="MAE threshold for tests")
+        parser.add_argument("--validate-all", action="store_true",
+                            help="Run tests for all challenger models and promote the best one to champion")
+        args = parser.parse_args()
+
+        giskard_dataset, df = create_giskard_dataset(args.data_version, cfg)
+
+        if args.validate_all:
+            print("Validating all challenger models")
+            best_model = validate_all_models(args.model_name, args.data_version, args.mae_threshold, giskard_dataset,
+                                             cfg)
+            if best_model is not None:
+                print(f"Best model: {best_model}")
+        else:
+            scan, test_results, model_version = validate_model(args.model_name, args.model_alias, args.model_version,
+                                                               args.data_version, args.mae_threshold, giskard_dataset,
+                                                               cfg)
+            print(f"Model version: {model_version}")
+            print(f"Scan issues: {len(scan.issues)}")
+            print(f"Test results: {test_results}")
+
 
 if __name__ == "__main__":
-    validate()
+    main()
