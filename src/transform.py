80316178b4a59b23771b371906afb4e2a58fbfbb
kama34 <kamishnikov98@gmail.com>
2024-07-29 02:07:45 +0300

    Merge with Sofya

---
+++ src/transform.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+current_directory = os.getcwd()
+sys.path.append(current_directory)
+from services.airflow.dags.data_prepare import prepare_data_pipeline
+
+
+def main():
+    print("Testing zenml pipeline")
+    prepare_data_pipeline()
+    print("Zenml pipeline successfully completed!")
+
+
+if __name__ == "__main__":
+    main()
