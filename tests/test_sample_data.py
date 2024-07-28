b69d2658cecbe0440969489caa13c0a53a8e8c2b
kama <kamishnikov98@gmail.com>
2024-07-28 18:14:47 +0300

    Improved test for sample_data PASSED [100%]

--- tests/test_sample_data.py
+++ tests/test_sample_data.py
@@ -5,7 +5,7 @@ import os
 
 from src.data import sample_data
 
-# Пример конфигурации, которую вы используете в функции
+# Example of configuration you use in the function
 mock_cfg = MagicMock()
 mock_cfg.data.path = 'data/samples/sample.csv'
 mock_cfg.data.remote = "localstore"
@@ -23,20 +23,20 @@ class TestSampleData(unittest.TestCase):
     @patch('src.data.pd.read_csv')
     @patch('src.data.os.system')
     def test_sample_data(self, mock_system, mock_read_csv, mock_get_url, mock_download_dataset, mock_compose):
-        # Настройка мока для compose
+        # Setting up a mock for compose
         mock_compose.return_value = mock_cfg
 
-        # Настройка мока для dvc.api.get_url
+        # Setting up a mock for dvc.api.get_url
         mock_get_url.return_value = 'mock_data_url'
 
-        # Создание фейковых данных для pandas.read_csv
+        # Create fake data for pandas.read_csv
         fake_data = pd.DataFrame({'col1': range(100)})
         mock_read_csv.return_value = fake_data
 
-        # Вызов тестируемой функции
+        # Call the function under test
         sample_data(mock_cfg)
 
-        # Проверка вызовов
+        # Call Check
         mock_download_dataset.assert_called_once()
         mock_get_url.assert_called_once_with(
             path=mock_cfg.data.path,
@@ -46,16 +46,16 @@ class TestSampleData(unittest.TestCase):
         )
         mock_read_csv.assert_called_once_with('mock_data_url')
 
-        # Проверка правильности сэмплирования данных
+        # Checking the correctness of data sampling
         expected_sample = fake_data.sample(frac=mock_cfg.data.sample_size, random_state=mock_cfg.data.random_state)
         pd.testing.assert_frame_equal(
             mock_read_csv.return_value.sample(frac=mock_cfg.data.sample_size, random_state=mock_cfg.data.random_state),
             expected_sample)
 
-        # Проверка, что директория и файл были созданы
+        # Check that the directory and file were created
         self.assertTrue(os.path.exists('data/samples/sample.csv'))
 
-        # Проверка вызовов os.system
+        # Checking os.system calls
         mock_system.assert_any_call('dvc add data/samples/sample.csv')
         mock_system.assert_any_call('dvc push')
 
